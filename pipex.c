a0260056244d8219163462353e7d8f5c062410b1
Elkan <echoo@42mail.sutd.edu.sg>
2026-01-14 22:56:17 +0800

    Seems to work! Now to handle other commands ...

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Elkan Choo <echoo@42mail.sutd.edu.sg>      +#+  +:+       +#+        */
+/*   By: elkan <elkan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:38:33 by elkan             #+#    #+#             */
-/*   Updated: 2026/01/14 18:12:34 by Elkan Choo       ###   ########.fr       */
+/*   Updated: 2026/01/14 22:53:23 by elkan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 int		open_file1(char *argv[], int *error, int *fail);
 int		open_file2(char *argv[], int *error, int *fail);
 void	setup(int argc, char *argv[], int *file1_fd, int *file2_fd);
-void	child_1(char *cmd, char *envp[], int pip_1, int file1_fd);
-void	child_2(char *cmd, char *envp[], int pip_0, int file2_fd);
+void	child_1(char *cmd, char *envp[], int pip[2], int file1_fd);
+void	child_2(char *cmd, char *envp[], int pip[2], int file2_fd);
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -32,8 +33,12 @@ int	main(int argc, char *argv[], char *envp[])
 
 	setup(argc, argv, &file1_fd, &file2_fd);
 	pipe(pip);
-	child_1(argv[2], envp, pip[1], file1_fd);
-	child_2(argv[3], envp, pip[0], file2_fd);
+	child_1(argv[2], envp, pip, file1_fd);
+	child_2(argv[3], envp, pip, file2_fd);
+	close(pip[0]);
+	close(pip[1]);
+	waitpid(file1_fd, NULL, 0);
+	waitpid(file2_fd, NULL, 0);
 }
 
 void	setup(int argc, char *argv[], int *file1_fd, int *file2_fd)
@@ -73,7 +78,7 @@ int	open_file2(char *argv[], int *error, int *fail)
 {
 	int	to_return;
 
-	to_return = open(argv[4], O_WRONLY, O_CREAT, O_TRUNC);
+	to_return = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (to_return < 0)
 	{
 		*error = 1;
@@ -84,7 +89,7 @@ int	open_file2(char *argv[], int *error, int *fail)
 	return (to_return);
 }
 
-void	child_1(char *cmd, char *envp[], int pip_1, int file1_fd)
+void	child_1(char *cmd, char *envp[], int pip[2], int file1_fd)
 {
 	pid_t	child;
 	char	*path;
@@ -95,9 +100,10 @@ void	child_1(char *cmd, char *envp[], int pip_1, int file1_fd)
 	if (child == 0)
 	{
 		dup2(file1_fd, 0);
-		dup2(pip_1, 1);
+		dup2(pip[1], 1);
 		close(file1_fd);
-		close(pip_1);
+		close(pip[1]);
+		close(pip[0]);
 		cmds = ft_split(cmd, ' ');
 		if (cmds == NULL)
 		{
@@ -105,13 +111,18 @@ void	child_1(char *cmd, char *envp[], int pip_1, int file1_fd)
 			perror("ft_split");
 			exit (1);
 		}
+		dprintf(2, "path: %s\n", path);
+		for (int i = 0; cmds[i]; i++)
+		{
+			dprintf(2, "cmds[%i]: %s\n", i, cmds[i]);
+		}
 		execve(path, cmds, envp);
 		perror("execve");
 		exit (1);
 	}
 }
 
-void	child_2(char *cmd, char *envp[], int pip_0, int file2_fd)
+void	child_2(char *cmd, char *envp[], int pip[2], int file2_fd)
 {
 	pid_t	child;
 	char	*path;
@@ -121,10 +132,11 @@ void	child_2(char *cmd, char *envp[], int pip_0, int file2_fd)
 	child = fork();
 	if (child == 0)
 	{
-		dup2(pip_0, 0);
+		dup2(pip[0], 0);
 		dup2(file2_fd, 1);
 		close(file2_fd);
-		close(pip_0);
+		close(pip[0]);
+		close(pip[1]);
 		cmds = ft_split(cmd, ' ');
 		if (cmds == NULL)
 		{
